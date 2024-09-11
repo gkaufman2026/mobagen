82615558b892ed9188ba1d922c4ca78eb5f6d988
Gerald Kaufman <TheCandyFreak2@gmail.com>
2024-09-11 16:10:41 -0400

    Didn't commit from class yesterday, redid

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -3,9 +3,21 @@
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
   // todo: implement
+
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
   // todo: implement
-  return 0;
+  int sumOfNeighbors = 0;
+  int row, column;
+
+  for (row = point.x - 1; row < 1; row++) {
+    for (column = point.y - 1; column < 1; column++) {
+      if (point.x != 1 && point.y != 1) {
+        const Point2D newPoint = point + Point2D{row, column};
+        sumOfNeighbors += world.Get(newPoint);
+      }
+    }
+  }
+  return sumOfNeighbors;
 }
