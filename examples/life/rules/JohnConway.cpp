1f99daf852abc9262369eec2ef81000b71d304d4
Gerald Kaufman <TheCandyFreak2@gmail.com>
2024-09-11 18:35:41 -0400

    Conway code semi works, bit confused

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -3,7 +3,24 @@
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
   // todo: implement
+  int row, column, currenNeighborCount;
+  bool isWithinSpace;
 
+  for (row = 0; row < world.SideSize(); row++) {
+    for (column = 0; column < world.SideSize(); column++) {
+      const Point2D newPoint = Point2D{row, column};
+      isWithinSpace = world.Get(newPoint);
+      currenNeighborCount = CountNeighbors(world, newPoint);
+
+      if (isWithinSpace) {
+        currenNeighborCount <= 3 ? world.SetNext(newPoint, true) : world.SetNext(newPoint, false);
+      } else {
+        if (currenNeighborCount == 3) {
+          world.SetNext(newPoint, true);
+        }
+      }
+    }
+  }
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
@@ -11,11 +28,11 @@ int JohnConway::CountNeighbors(World& world, Point2D point) {
   int sumOfNeighbors = 0;
   int row, column;
 
-  for (row = point.x - 1; row < 1; row++) {
-    for (column = point.y - 1; column < 1; column++) {
-      if (point.x != 1 && point.y != 1) {
-        const Point2D newPoint = point + Point2D{row, column};
-        sumOfNeighbors += world.Get(newPoint);
+  for (row = point.x - 1; row < point.x + 2; row++) {
+    for (column = point.y - 1; column < point.y + 2; column++) {
+      if (point.x != 1 || point.y != 1) {
+        const Point2D newPoint = Point2D{row, column};
+        sumOfNeighbors += (int) world.Get(newPoint);
       }
     }
   }
