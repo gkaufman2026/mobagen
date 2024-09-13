107cf38f6c0af21e5a5905ae314c82419f1d24ef
gkaufman2026 <gerald.kaufman@mymail.champlain.edu>
2024-09-13 13:18:42 -0400

    Added comments

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -13,6 +13,8 @@ void JohnConway::Step(World& world) {
       currenNeighborCount = CountNeighbors(world, newPoint);
 
       if (isWithinSpace) {
+        // Checking if currentNeighborCount is <= 3, if true, will allow neighbors to live on to next gen
+        // If false, step 1 & 3 are included in bool if ! <= 3
         currenNeighborCount <= 3 ? world.SetNext(newPoint, true) : world.SetNext(newPoint, false);
       } else {
         if (currenNeighborCount == 3) {
@@ -28,10 +30,13 @@ int JohnConway::CountNeighbors(World& world, Point2D point) {
   int sumOfNeighbors = 0;
   int row, column;
 
+  // Parsing through each row and column
   for (row = point.x - 1; row < point.x + 2; row++) {
     for (column = point.y - 1; column < point.y + 2; column++) {
+      // If x or y is != to for index of row or column
       if (point.x != row || point.y != column) {
         const Point2D newPoint = Point2D{row, column};
+        // sets true to 1 and adds to sumOfNeighbors variable
         sumOfNeighbors += (int) world.Get(newPoint);
       }
     }
