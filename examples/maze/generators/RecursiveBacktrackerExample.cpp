06ce4fb9403d5072596c46f96abd12f1c35ff586
gkaufman2026 <gerald.kaufman@mymail.champlain.edu>
2024-09-24 14:17:22 -0400

    Working :D

--- examples/maze/generators/RecursiveBacktrackerExample.cpp
+++ examples/maze/generators/RecursiveBacktrackerExample.cpp
@@ -2,8 +2,12 @@
 #include "Random.h"
 #include "RecursiveBacktrackerExample.h"
 #include <climits>
+
 bool RecursiveBacktrackerExample::Step(World* w) {
   // todo: implement this
+  if (stack.empty && visited.empty()) {
+    stack.push_back(randomStartPoint(w));
+  }
   return false;
 }
 
@@ -11,7 +15,6 @@ void RecursiveBacktrackerExample::Clear(World* world) {
   visited.clear();
   stack.clear();
   auto sideOver2 = world->GetSize() / 2;
-
   for (int i = -sideOver2; i <= sideOver2; i++) {
     for (int j = -sideOver2; j <= sideOver2; j++) {
       visited[i][j] = false;
@@ -21,11 +24,7 @@ void RecursiveBacktrackerExample::Clear(World* world) {
 
 Point2D RecursiveBacktrackerExample::randomStartPoint(World* world) {
   auto sideOver2 = world->GetSize() / 2;
-
   // todo: change this if you want
-  for (int y = -sideOver2; y <= sideOver2; y++)
-    for (int x = -sideOver2; x <= sideOver2; x++)
-      if (!visited[y][x]) return {x, y};
   return {INT_MAX, INT_MAX};
 }
 
@@ -33,7 +32,13 @@ std::vector<Point2D> RecursiveBacktrackerExample::getVisitables(World* w, const 
   auto sideOver2 = w->GetSize() / 2;
   std::vector<Point2D> visitables;
 
-  // todo: implement this
+  if (p.x -1 >= -sideOver2 || p.y - 1 >= -sideOver2) {
+    visitables.push_back(Point2D(p.x - 1, p.y));
+  }
+
+  if (p.x + 1 <= sideOver2 || p.y + 1 <= sideOver2) {
+    visitables.push_back(Point2D(p.x, p.y + 1));
+  }
 
   return visitables;
 }
