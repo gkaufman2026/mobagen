a4dd36666e1fff3772b2b388baf3ba4e96748100
gkaufman2026 <gerald.kaufman@mymail.champlain.edu>
2024-10-08 13:59:33 -0400

    Classtime

--- examples/catchthecat/Agent.cpp
+++ examples/catchthecat/Agent.cpp
@@ -4,21 +4,13 @@
 #include <queue>
 #include "World.h"
 using namespace std;
-std::vector<Point2D> Agent::getVisitedNeighbors(World* w, const Point2D& p) {
-  std::vector deltas = {Point2D::UP, Point2D::DOWN, Point2D::LEFT, Point2D::RIGHT};
-  auto sideOver2 = w->getWorldSideSize() / 2;
-  std::vector<Point2D> neighbors;
-
-  // todo: code this
-
-  return neighbors;
-}
 
 std::vector<Point2D> Agent::generatePath(World* w) {
   unordered_map<Point2D, Point2D> cameFrom;  // to build the flowfield and build the path
   queue<Point2D> frontier;                   // to store next ones to visit
   unordered_set<Point2D> frontierSet;        // OPTIMIZATION to check faster if a point is in the queue
   unordered_map<Point2D, bool> visited;      // use .at() to get data, if the element dont exist [] will give you wrong results
+  vector<Point2D> path, neighbors;
 
   // bootstrap state
   auto catPos = w->getCat();
@@ -27,29 +19,62 @@ std::vector<Point2D> Agent::generatePath(World* w) {
   Point2D borderExit = Point2D::INFINITE;  // if at the end of the loop we dont find a border, we have to return random points
 
   while (!frontier.empty()) {
-    // get the current from frontier
     Point2D current = frontier.front();
-    // remove the current from frontierset
     frontierSet.erase(current);
-    // mark current as visited
     visited[current] = true;
-    // getVisitableNeightbors(world, current) returns a vector of neighbors that are not visited, not cat, not block, not in the queue
-    vector<Point2D> neighbors = getVisitedNeighbors(w, current);
-    // iterate over the neighs:
-    // for every neighbor set the cameFrom
+    neighbors = getVisitedNeighbors(w, current, frontierSet, visited);
     for (auto neighbor : neighbors) {
-
+      cameFrom[neighbor] = current;
+      frontierSet.insert(neighbor);
+      frontier.push(neighbor);
     }
-
-    // enqueue the neighbors to frontier and frontierset
-
-    // do this up to find a visitable border and break the loop
-
   }
 
-  // if the border is not infinity, build the path from border to the cat using the camefrom map
+  if (borderExit != Point2D::INFINITE) {
+    Point2D current = borderExit;
+    while (current != catPos) {
+      current = cameFrom[current];
+      path.push_back(current);
+    }
+  }
 
-  // if there isnt a reachable border, just return empty vector
-  // if your vector is filled from the border to the cat, the first element is the catcher move, and the last element is the cat move
-  return vector<Point2D>();
+  return path;
+}
+
+bool Agent::isCheckPointFound(Point2D check, World* w, std::unordered_set<Point2D>& frontierSet, std::unordered_map<Point2D, bool>& visited) {
+  return !visited.contains(check) && w->getCat() != check && !frontierSet.contains(check) && !w->getContent(check);
+}
+
+std::vector<Point2D> Agent::getVisitedNeighbors(World* w, const Point2D& p, unordered_set<Point2D>& frontierSet, unordered_map<Point2D, bool>& visited) {
+  int startPoint = p.x - static_cast<int>(p.y % 2 == 0);
+  std::vector<Point2D> neighbors;
+
+  // Checking above
+  for (int i = startPoint; i < startPoint + 2; i++) {
+    const Point2D checkSpot = {i, p.y - 1};
+    if (isCheckPointFound(checkSpot, w, frontierSet, visited)) {
+      neighbors.push_back(checkSpot);
+    }
+  }
+
+  // Checking below
+  for (int i = startPoint; i < startPoint + 2; i++) {
+    const Point2D checkSpot = {i, p.y + 1};
+    if (isCheckPointFound(checkSpot, w, frontierSet, visited)) {
+      neighbors.push_back(checkSpot);
+    }
+  }
+
+  // Checking sides
+  startPoint = p.x - 1;
+  for (int i = startPoint; i < startPoint + 3; i++) {
+    if (i != p.x) {
+      const Point2D checkSpot = {i, p.y};
+      if (isCheckPointFound(checkSpot, w, frontierSet, visited)) {
+        neighbors.push_back(checkSpot);
+      }
+    }
+  }
+
+  return neighbors;
 }
--- examples/catchthecat/Agent.h
+++ examples/catchthecat/Agent.h
@@ -1,6 +1,8 @@
 #ifndef AGENT_H
 #define AGENT_H
 #include "math/Point2D.h"
+#include <unordered_set>
+#include <unordered_map>
 #include <vector>
 
 class World;
@@ -10,8 +12,9 @@ public:
   explicit Agent() = default;
 
   virtual Point2D Move(World*) = 0;
-  std::vector<Point2D> getVisitedNeighbors(World* w, const Point2D& p);
+  std::vector<Point2D> getVisitedNeighbors(World* w, const Point2D& p, std::unordered_set<Point2D> &frontierSet, std::unordered_map<Point2D, bool> &visited);
   std::vector<Point2D> generatePath(World* w);
+  bool isCheckPointFound(Point2D check, World* w, std::unordered_set<Point2D> &frontierSet, std::unordered_map<Point2D, bool> &visited);
 };
 
 #endif  // AGENT_H
